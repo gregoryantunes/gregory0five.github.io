822cbaca71b0fe4a8ff5f768865f335ece32bad0
gregory0five <68088354+gregory0five@users.noreply.github.com>
2022-06-02 17:40:42 -0300

    Delete crontab-install.sh

--- script/crontab-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum -y install cronie
-
-crontab -l > mycron
-echo "* * * * * /bin/bash /root/zabbix-scripts/monitoring" >> mycron
-echo "*/5 * * * * /bin/bash /root/zabbix-scripts/zabbix-conf.sh" >> mycron
-crontab mycron
