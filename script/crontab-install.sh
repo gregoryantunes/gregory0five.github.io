f8abfcba429445c934e7234a1f267c4dd8099005
gregory0five <68088354+gregory0five@users.noreply.github.com>
2022-06-02 17:37:36 -0300

    Update crontab-install.sh

--- script/crontab-install.sh
+++ script/crontab-install.sh
@@ -3,6 +3,6 @@
 yum -y install cronie
 
 crontab -l > mycron
-echo "* * * * * /bin/bash /root/script/monitoring" >> mycron
-echo "*/5 * * * * /bin/bash /root/script/zabbix-conf.sh" >> mycron
+echo "* * * * * /bin/bash /root/zabbix-scripts/monitoring" >> mycron
+echo "*/5 * * * * /bin/bash /root/zabbix-scripts/zabbix-conf.sh" >> mycron
 crontab mycron
