5d8675f4c33cf8a9ab127196516bd87983baeeef
gregory0five <68088354+gregory0five@users.noreply.github.com>
2022-06-03 17:05:22 -0300

    Add files via upload

---
+++ script/cron-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#yum -y install cronie
+
+#crontab -l > mycron
+#echo "* * * * * /bin/bash /root/script/monitoring" >> mycron
+#echo "*/5 * * * * /bin/bash /root/script/zabbix-conf.sh" >> mycron
+#crontab mycron
+
+crontab -l > mycron
+echo "* * * * * /bin/bash /root/script/monitoring
+*/5 * * * * /bin/bash /root/script/zabbix-conf.sh" > mycron
+crontab mycron
