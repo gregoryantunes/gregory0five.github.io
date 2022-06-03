1f743c63e1b631a86d678d872bf6b0e240dd40fe
gregory0five <68088354+gregory0five@users.noreply.github.com>
2022-06-03 17:04:35 -0300

    Delete script directory

--- script/cron-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum -y install cronie
-
-crontab -l > mycron
-echo "* * * * * /bin/bash /root/script/monitoring" >> mycron
-echo "*/5 * * * * /bin/bash /root/script/zabbix-conf.sh" >> mycron
-crontab mycron
